cbee64bdfff6bc120474dde26ce044b1170d0ea4
tonynus <92866439+tonynus@users.noreply.github.com>
2021-10-21 21:32:29 +0800

    Update create_co2_table.sql

--- create_co2_table.sql
+++ create_co2_table.sql
@@ -2,4 +2,7 @@ CREATE TABLE co2emission_reduced (
     imo BIGINT PRIMARY KEY,
     ship_name VARCHAR(64) NOT NULL,
     technical_efficiency_number REAL
+    ship_type VARCHAR(64) NOT NULL,
+    issue_date VARCHAR(64) NOT NULL,
+    expiry_date VARCHAR(64) NOT NULL
 );
