e2cafa53e9e467f74c3639c36d0811b39e186aa6
tonynus <92866439+tonynus@users.noreply.github.com>
2021-10-22 01:17:19 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -17,6 +17,14 @@ COLUMNS = [
     'expiry_date'
 ]
 
+COLUMNS_1 = [
+    'ship_type',
+    'count',
+    'minimum',
+    'average',
+    'maximum'
+]
+
 
 def index(request):
     """Shows the main page"""
@@ -63,6 +71,42 @@ def emissions(request, page=1):
 
     context = {
         'nbar': 'emissions',
+        'page': page,
+        'rows': rows,
+        'num_pages': num_pages,
+        'msg': msg,
+        'order_by': order_by
+    }
+    return render(request, 'emissions.html', context)
+
+def aggregation(request, page=1):
+    """Shows the aggregation table page"""
+    msg = None
+    order_by = request.GET.get('order_by', '')
+    order_by = order_by if order_by in COLUMNS_1 else 'imo'
+
+    with connections['default'].cursor() as cursor:
+        cursor.execute('SELECT COUNT(*) FROM co2emission_reduced')
+        count = cursor.fetchone()[0]
+        num_pages = (count - 1) // PAGE_SIZE + 1
+        page = clamp(page, 1, num_pages)
+
+        offset = (page - 1) * PAGE_SIZE
+        cursor.execute(f'''
+            SELECT {", ".join(COLUMNS_1)}
+            FROM co2emission_reduced
+            ORDER BY {order_by}
+            OFFSET %s
+            LIMIT %s
+        ''', [offset, PAGE_SIZE])
+        rows = namedtuplefetchall(cursor)
+
+    imo_deleted = request.GET.get('deleted', False)
+    if imo_deleted:
+        msg = f'✔ IMO {imo_deleted} deleted'
+
+    context = {
+        'nbar': 'aggregation',
         'page': page,
         'rows': rows,
         'num_pages': num_pages,
